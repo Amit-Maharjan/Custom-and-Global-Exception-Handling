a36cd055223e685fb62c04117a05a712b31103a9
Amit Maharjan <maharjanamit8@gmail.com>
2025-08-23 18:51:26 -0400

    Initial project from Spring Initializr

---
+++ src/test/java/com/maharjan/exceptionHandling/ExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maharjan.exceptionHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
