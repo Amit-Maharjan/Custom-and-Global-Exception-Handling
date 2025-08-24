c87664d5842158dbcf937d3e5ca4a931bb0db0ac
Amit Maharjan <maharjanamit8@gmail.com>
2025-08-24 13:52:38 -0400

    Created controller and service for global exception.

---
+++ src/main/java/com/maharjan/exceptionHandling/controllers/GlobalExceptionEmployeeController.java
@@ -0,0 +1,47 @@
+package com.maharjan.exceptionHandling.controllers;
+
+import com.maharjan.exceptionHandling.entity.Employee;
+import com.maharjan.exceptionHandling.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee/global")
+public class GlobalExceptionEmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employeeList = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.saveEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployeeById(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updatedEmployee = employeeService.saveEmployee(employee);
+        return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/maharjan/exceptionHandling/services/GlobalExceptionEployeeService.java
@@ -0,0 +1,30 @@
+package com.maharjan.exceptionHandling.services;
+
+import com.maharjan.exceptionHandling.entity.Employee;
+import com.maharjan.exceptionHandling.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GlobalExceptionEployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployeeById(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
